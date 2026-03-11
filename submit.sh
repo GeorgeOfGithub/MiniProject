df65d58d1028740bed6d3bfe182f5e2add900cc0
GeorgeOfGithub <s214691@dtu.dk>
2026-03-11 09:18:27 +0100

    added submit script and gitignore

---
+++ submit.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#BSUB -J python
+#BSUB -q hpc
+#BSUB -W 15
+#BSUB -n 12
+#BSUB -R "span[hosts=1]"
+#BSUB -R "select[model == XeonGold6226R]"
+#BSUB -R "rusage[mem=1GB]"
+#BSUB -o batch_output/python_%J.out
+#BSUB -e batch_output/python_%J.err
+
+# Initialize Python environment
+source /dtu/projects/02613_2025/conda/conda_init.sh
+conda activate 02613
+
+# Run Python script
+python Optimized.py 1
